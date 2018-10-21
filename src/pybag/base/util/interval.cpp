7e5d8d75db6fe55b7d0e57b805f4b6c2272e3712
Eric Chang <pkerichang@gmail.com>
2018-10-21 14:22:55 -0700

    update PyDisjointInterval documentation.

--- src/pybag/base/util/interval.cpp
+++ src/pybag/base/util/interval.cpp
@@ -1,3 +1,5 @@
+#include <memory>
+
 #include <cbag/util/interval.h>
 
 #include <pybag/base/util/interval.h>
@@ -24,12 +26,35 @@ class py_interval : py_interval_base {
 
 namespace pu = pybag::util;
 
+using c_dis_intvs = cu::disjoint_intvs<pu::py_interval>;
+
 void bind_util_interval(py::module &m_util) {
     py::module m = m_util.def_submodule("interval");
     m.doc() = "This module contains utility classes for manipulating intervals.";
 
-    auto py_dis_intvs = py::class_<cu::disjoint_intvs<pu::py_interval>>(m, "PyDisjointIntervals");
-    py_dis_intvs.doc() = "A class that keeps track of disjoint intervals.";
+    auto py_dis_intvs = py::class_<c_dis_intvs>(m, "PyDisjointIntervals");
+    py_dis_intvs.doc() = R"mydelim(A class that keeps track of disjoint intervals.
 
-    py_dis_intvs.def(py::init<>());
+Parameters
+----------
+intv_list : Optional[List[Tuple[int, int]]]
+    list of intervals.
+val_list : Optional[List[Any]]
+    list of values.
+)mydelim";
+
+    py::options options;
+    options.disable_function_signatures();
+
+    const char *init_doc = "__init__(self, "
+                           "intv_list: Optional[List[Tuple[int, int]]]=None, "
+                           "val_list: Optional[List[Any]]=None) -> None";
+    py_dis_intvs.def(py::init([](py::list intv_list, py::list val_list) {
+                         auto ans = std::make_unique<c_dis_intvs>();
+                         return ans;
+                     }),
+                     init_doc, py::arg_v("intv_list", nullptr).none(true),
+                     py::arg_v("val_list", nullptr).none(true));
+
+    options.enable_function_signatures();
 }
