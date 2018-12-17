2ed6f6fe8cb927fe83d6cf0f0af9c61f970421da
Eric Chang <pkerichang@gmail.com>
2018-12-17 01:58:13 -0800

    Update cbag version and Transform object bindings.

--- src/pybag/transform.cpp
+++ src/pybag/transform.cpp
@@ -31,4 +31,12 @@ void bind_transform(py::module &m) {
     py_cls.def_property_readonly("y", &c_transform::y, "Y shift.");
     py_cls.def_property_readonly("orient", &get_xform_orient, "Orientation.");
     py_cls.def_property_readonly("location", &get_xform_location, "Location.");
+    py_cls.def("move_by", &c_transform::move_by, "Moves this transform object", py::arg("dx") = 0,
+               py::arg("dy") = 0);
+    py_cls.def("get_move_by", &c_transform::get_move_by, "Get a shifted transform object",
+               py::arg("dx") = 0, py::arg("dy") = 0);
+    py_cls.def("transform_by", &c_transform::transform_by, "Transforms this transform object",
+               py::arg("xform"));
+    py_cls.def("get_transform_by", &c_transform::get_transform_by,
+               "Returns a transformed transform object", py::arg("xform"));
 }
