658af15d32544e77d2301bacffe81568f2a1fbb0
Eric Chang <pkerichang@gmail.com>
2018-12-28 01:13:50 +0800

    Update tech class bindings

--- src/pybag/tech.cpp
+++ src/pybag/tech.cpp
@@ -1,6 +1,6 @@
 #include <pybind11/pybind11.h>
 
-#include <cbag/layout/tech.h>
+#include <cbag/layout/tech_util.h>
 
 #include <pybag/tech.h>
 
@@ -14,13 +14,11 @@ void bind_tech(py::module &m) {
     py_cls.def(py::init<std::string>(), "Create a new PyTech class from file.",
                py::arg("tech_fname"));
 
-    py_cls.def_property_readonly("pin_purpose", &c_tech::get_pin_purpose_name,
+    py_cls.def_property_readonly("pin_purpose", &cbag::layout::get_pin_purpose_name,
                                  "The pin purpose name.");
-    py_cls.def_property_readonly("default_purpose", &c_tech::get_default_purpose_name,
+    py_cls.def_property_readonly("default_purpose", &cbag::layout::get_default_purpose_name,
                                  "The default purpose name.");
-    py_cls.def("get_min_space",
-               py::overload_cast<const std::string &, cbag::offset_t, cbag::enum_t>(
-                   &c_tech::get_min_space, py::const_),
+    py_cls.def("get_min_space", cbag::layout::get_min_space,
                "Returns the minimum required spacing.", py::arg("layer_type"), py::arg("width"),
                py::arg("sp_type"));
 }
