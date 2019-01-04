940ba565cf213faf68655358c2375ebbdfcb8ca3
Eric Chang <pkerichang@gmail.com>
2019-01-04 01:10:44 -0800

    update cbag version and python bindings.

--- src/pybag/layout.cpp
+++ src/pybag/layout.cpp
@@ -139,7 +139,7 @@ void bind_cellview(py::module &m) {
                py::arg("bnd_code"), py::arg("points"), py::arg("commit"));
     py_cls.def("add_pin", &c_cellview::add_pin, "Adds a pin object.", py::arg("layer"),
                py::arg("net"), py::arg("label"), py::arg("bbox"));
-    py_cls.def("add_label", &c_cellview::add_label, "Adds a label object.", py::arg("layer"),
+    py_cls.def("add_label", &cbag::layout::add_label, "Adds a label object.", py::arg("layer"),
                py::arg("purpose"), py::arg("xform"), py::arg("label"));
     py_cls.def("add_path", &cbag::layout::add_path<py_pt_vector>, "Adds a new path.",
                py::arg("layer"), py::arg("purpose"), py::arg("is_horiz"), py::arg("points"),
